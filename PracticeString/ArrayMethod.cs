256134c4bbbaf5e7f921c602d7a29ad1cf7c470e
admin <admin@DESKTOP-2QEG8VG>
2024-10-08 03:20:01 -0700

    Assignment 2 Array practice

---
+++ PracticeString/ArrayMethod.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeString
+{
+    internal class ArrayMethod
+    {
+        public void ListManager()
+        {
+            List<string> to_do = new List<string>();
+            string inputCommand;
+
+            while (true)
+            {
+                if (to_do.Count > 0)
+                {
+                    Console.Write("The current to do list: ");
+                    Console.WriteLine(string.Join(", ", to_do));
+                }
+                else
+                {
+                    Console.WriteLine("The list is Empty.");
+                }
+
+                Console.WriteLine("Enter command (+ item, - item, or -- to clear)):");
+                inputCommand = Console.ReadLine();
+
+                switch (inputCommand)
+                {
+                    case "+":
+                        Console.WriteLine("Enter what to do: ");
+                        string inputToDo = Console.ReadLine();
+                        to_do.Add(inputToDo);
+                        break;
+                    case "-":
+                        Console.WriteLine("Choose which to be removed:");
+                        string inputToRemove = Console.ReadLine();
+                        if (to_do.Remove(inputToRemove))
+                            Console.WriteLine("Has been removed.");
+                        else
+                            Console.WriteLine("Can not find it.");
+                        break;
+                    case "--":
+                        to_do.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("End the list manage.");
+                        return;
+
+                }
+            }
+        }
+
+        public int[] FindPrimesInRange(int starNum, int endNum)
+        {
+            List<int> primeList = new List<int>();  
+            for (int i = starNum; i <= endNum; i++)
+            {
+                if(IsPrime(i)) primeList.Add(i);
+            }
+            return primeList.ToArray();
+        }
+
+        // check whether the number is prime
+        public bool IsPrime(int num)
+        {
+            if (num <= 1) return false;
+
+            for (int i = 2; i * i <= num; i++)
+            {
+                if(num % i == 0) return false;
+            }
+            return true;
+        }
+
+        public int[] RotateArray(int[] array, int k)
+        {
+            int n = array.Length;
+            int[] sum = new int[n];
+
+            for (int i = 1; i <= k; i++)
+            {
+                int[] temp = new int[n];
+                array.CopyTo(temp, 0);
+                ReverseArray(temp, 0, n - 1);
+                ReverseArray(temp, 0, i - 1);
+                ReverseArray(temp, i, n - 1);
+                for(int j = 0; j < n; j++)
+                {
+                    sum[j] += temp[j];
+                }
+                
+            }
+            return sum;
+        }
+
+        // reverse the array from start to end
+        public void ReverseArray(int[] arr, int start, int end)
+        {
+            while(start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+
+        }
+
+        public int[] FindTheLongestSequence(int[] arr)
+        {
+            int longestStart = 0, curStart = 0, longestLength = 0;
+            
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] == arr[i - 1])
+                {
+                    if(i - curStart + 1 > longestLength)
+                    {
+                        longestLength = i - curStart + 1;
+                        longestStart = curStart;
+                    }
+                }    
+                else
+                {
+                    curStart = i;
+                }
+                
+            }
+
+            int[] result = new int[longestLength];
+
+            for(int j = 0; j < longestLength; j++)
+            {
+                result[j] = arr[longestStart++];
+            }
+            return result;
+        }
+
+        public int FindMostFrequentNumber(int[] arr)
+        {
+            Dictionary<int, int> countDic = new Dictionary<int, int>();
+            int max = 0;
+
+            foreach(int i in arr)
+            {
+                if (countDic.ContainsKey(i)) countDic[i]++;
+                else countDic[i] = 1;
+
+                max = Math.Max(max, countDic[i]);
+            }
+
+            foreach(int i in arr)
+            {
+                if (countDic[i] == max) return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ PracticeString/Program.cs
@@ -0,0 +1,89 @@
+﻿/* 1. Copying an Array
+Write code to create a copy of an array. First, start by creating an initial array. (You can use
+whatever type of data you want.) Let’s start with 10 items. Declare an array variable and
+assign it a new array with 10 items in it. Use the things we’ve discussed to put some values
+in the array.
+Now create a second array variable. Give it a new array with the same length as the first.
+Instead of using a number for this length, use the Lengthproperty to get the size of the
+original array.
+Use a loop to read values from the original array and place them in the new array. Also
+print out the contents of both arrays, to be sure everything copied correctly. */
+
+using PracticeString;
+
+Console.WriteLine("Question1: ");
+
+int[] initialArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+int[] firstArray = new int[10];
+initialArray.CopyTo( firstArray, 0 );
+Console.WriteLine("firstArray:");
+Console.WriteLine(string.Join(", ", firstArray));
+
+int[] secondArray = new int[initialArray.Length];
+for (int i = 0; i < initialArray.Length; i++)
+{
+    secondArray[i] = initialArray[i];
+}
+Console.WriteLine("secondArray:");
+Console.WriteLine(string.Join(", ", secondArray));
+
+/* 2. Write a simple program that lets the user manage a list of elements. It can be a grocery list,
+"to do" list, etc. Refer to Looping Based on a Logical Expression if necessary to see how to
+implement an infinite loop. Each time through the loop, ask the user to perform an
+operation, and then show the current contents of their list. The operations available should
+be Add, Remove, and Clear.  
+Your program should read in the user's input and determine if it begins with a “+” or “-“ or
+if it is simply “—“ . In the first two cases, your program should add or remove the string
+given ("some item" in the example). If the user enters just “—“ then the program should
+clear the current list.  */
+Console.WriteLine("Question2: ");
+ArrayMethod method = new ArrayMethod();
+method.ListManager();
+
+/* 3. Write a method that calculates all prime numbers in given range and returns them as array
+of integers */
+Console.WriteLine("Question3: ");
+int[] primes = method.FindPrimesInRange(5, 67);
+Console.WriteLine(string.Join(", ", primes));
+
+/* 4. Write a program to read an array of n integers (space separated on a single line) and an
+integer k, rotate the array right k times and sum the obtained arrays after each rotation as
+shown below.
+After r rotations the element at position I goes to position (I + r) % n.
+The sum[] array can be calculated by two nested loops: for r = 1 ... k; for I = 0 ... n-1  */
+Console.WriteLine("Question4: ");
+
+Console.WriteLine("Enter an array separated by space: ");
+string input = Console.ReadLine();
+string[] inputStrArr = input.Split(' ');
+int[] inputIntArr = Array.ConvertAll(inputStrArr, int.Parse);
+
+Console.WriteLine("Enter the rotate times: ");
+int times = int.Parse(Console.ReadLine());
+
+int[] rotateSum = method.RotateArray(inputIntArr, times);
+Console.WriteLine(string.Join(", ", rotateSum));
+
+/* 5. Write a program that finds the longest sequence of equal elements in an array of integers.
+If several longest sequences exist, print the leftmost one.  */
+Console.WriteLine("Question5: ");
+
+Console.WriteLine("Enter an array separated by space: ");
+string input2 = Console.ReadLine();
+string[] inputStrArr2 = input2.Split(' ');
+int[] inputIntArr2 = Array.ConvertAll(inputStrArr2, int.Parse);
+
+int[] findLongestSequence = method.FindTheLongestSequence(inputIntArr2);
+Console.WriteLine(string.Join(", ", findLongestSequence));
+
+/* 7. Write a program that finds the most frequent number in a given sequence of numbers. In
+case of multiple numbers with the same maximal frequency, print the leftmost of them  */
+Console.WriteLine("Question7: ");
+
+Console.WriteLine("Enter an array separated by space: ");
+string input3 = Console.ReadLine();
+string[] inputStrArr3 = input3.Split(' ');
+int[] inputIntArr3 = Array.ConvertAll(inputStrArr3, int.Parse);
+
+int result = method.FindMostFrequentNumber(inputIntArr3);
+Console.WriteLine($"The leftmost most frequent number in this array is {result}");
